a36cd9c511f31da3816a97e919cd5df05e13ce01
Ahmad Yassin <ahmadyassin4521@gmail.com>
2025-11-09 18:04:36 +0700

    dalit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
    * @var array<int, string>
    */
   protected $fillable = [
-    'name', // Menggunakan 'name' sesuai model Anda
+    'name',
     'email',
     'password',
     'email_personal',
